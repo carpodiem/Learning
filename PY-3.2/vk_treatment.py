e27399afab5d1c24bb437af4476bbbb08c69d29c
carpodiem <e-sterh@inbox.ru>
2017-03-30 11:57:37 +0300

    Задача 3.2 пока без графов

--- PY-3.2/vk_treatment.py
+++ PY-3.2/vk_treatment.py
@@ -27,18 +27,20 @@ params = {'access_token': access_token,
           'v': VERSION,
           }
 
+# Получает список друзей пользователя
 def get_friends_of_friend(user_id = None):
     if user_id:
         params['user_id'] = user_id
-    params['fields'] = 'mickname'
+    params['fields'] = 'nickname'
     response = requests.get('https://api.vk.com/method/friends.get', params)
     return response.json()
 
+# Получает список моих друзей из ВК
 def friendslist_get():
-    params['fields'] = 'mickname'
+    params['fields'] = 'nickname'
     response = requests.get('https://api.vk.com/method/friends.get', params)
-    pprint(response.json())
 
+# Возвращает словарь без пользателей, которые удалены или деактивированы
 def connections():
     import time
     my_friends_list = get_friends_of_friend()
@@ -48,46 +50,48 @@ def connections():
             pprint(friend)
             friend_name = friend['first_name'] + ' ' + friend['last_name']
             friends_of_friends[friend_name] = get_friends_of_friend(friend['id'])
-        time.sleep(1)
-    return friends_of_friends
+        time.sleep(1) # Задержка в секунду, так как ВК не позволяет делать больше 1000 запросов в секунду
+        return friends_of_friends
 
+# Записываем полученный список в файл, чтобы не отправлять постоянные запросы в ВК
 def list_to_file(list_of_json_friends):
     import json
     with open('friends_list.json','w', encoding = 'utf_8') as friends_file:
         json.dump(list_of_json_friends, friends_file, ensure_ascii=False, indent=2)
 
+# Возвращает отформатированый словарь с пользователями и их друзьями из файла
 def open_friends_file():
     import json
     with open('friends_list.json', 'r', encoding='utf_8') as f:
         friends = json.load(f)
-    return friends
+        friends_treated = dict()
+        for account_name, acc_friends in friends.items():
+            ids_set = set()
+            for user in acc_friends['response']['items']:
+                ids_set.add((user['id'], user['first_name'], user['last_name']))
+            friends_treated[account_name] = ids_set
+    return friends_treated
 
-def cross_checking(accounts_list, my_friends_connections):
-    print(type(my_friends_connections))
-    print(type(accounts_list))
-    for friend_name, friends in my_friends_connections.items():
-        crossing = set(friends['response']['items']) & accounts_list
-        if  crossing != []:
-            print(crossing)
-            return_crossing
+# Выводим на экран информацию о пересечении друзей моих друзей
+def print_friends(friends_crossings):
+    for crossfriend in friends_crossings:
+        print('Друзья {0} и {1} имеют общих друзей {2}' .format(crossfriend[0][0], crossfriend[0][1], crossfriend[1]))
 
+# Находит пересечения друзей между друзьями
 def cross_connections(friends_connections):
-    for account_name, account in friends_connections.items():
-        friends = account['response']['items']
-        crossed_friends = cross_checking(friends, friends_connections)
-        print(crossed_friends)
+    crossconnections = set()
+    for account_name, ids in friends_connections.items():
+        new_friends_list = friends_connections
+        for name, id_search in friends_connections.items():
+            crossing = []
+            if name != account_name:
+                crossing = list(id_search & ids)
+            if crossing != [] and crossing[0][0] != 77847:
+                cross_name = ((account_name, name),len(crossing))
+                crossconnections.add(cross_name)
+    return crossconnections
 
-
-# def print_friends(friends_of_friends_list):
-#     # friends_of_friends_list = friends_of_friends_list.read().decode('cp1251')
-#     for friend_name, friend in friends_of_friends_list.items():
-#         print('Друзья', friend_name, ':')
-#         for friend_of_friend in friend['response']['items']:
-#             # pprint(friend['response']['items'])
-#             print('   ', friend_of_friend['first_name'], '', friend_of_friend['last_name'], ' ', friend_of_friend['id'])
-
-
-# list_of_connections = connections()
-# list_to_file(list_of_connections)
-# print_friends(open_friends_file())
-cross_connections(open_friends_file())
+list_of_connections = connections()
+list_to_file(list_of_connections)
+crossings = cross_connections(open_friends_file())
+print_friends(crossings)
