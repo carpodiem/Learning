06d20fa68806017c55d33af0faa688ab3a7a3724
carpodiem <e-sterh@inbox.ru>
2017-04-03 16:05:17 +0300

    2.6 update

--- PY-2.6/image_resize.py
+++ PY-2.6/image_resize.py
@@ -1,32 +1,22 @@
+import subprocess, os, glob
 def resize_image(file_names):
-    import subprocess, os
     result_dir = os.path.join(os.getcwd(), 'Result')
     if 'Result' not in os.listdir(os.getcwd()):
         os.mkdir(result_dir)
     for image in file_names:
-        output_file = result_dir + '\\' + image.split('\\')[1] + '_res200.jpg'
-        program_w_args = 'convert.exe ' + image + ' -resize 200 ' + output_file
-        print(image.split('\\')[1])
+        output_file = os.path.join(result_dir, os.path.basename(os.path.splitext(image)[0]) + '_res200.jpg')
+        program_args = os.path.join(os.path.join(os.getcwd(),image + ' -resize 200 ' + output_file))
+        program_w_args = os.path.join(os.getcwd(), 'convert.exe ' + program_args)
         resize_it = subprocess.run(program_w_args)
+        print(os.path.basename(image))
         if resize_it.returncode == 0:
             print('Выполнено успешно!')
         else:
             print('Упс! Что-то пошло не так! Обретитесь к разработчкику.')
 
 def list_of_files(source_dir):
-    import glob, os.path
     image_files = glob.glob(os.path.join(source_dir, '*.jpg'))
     return image_files
 
-# def image_threding(images):
-#     import subprocess, os
-#     result_dir = os.path.join(os.getcwd(), 'Result')
-#     print(result_dir)
-#     from multiprocessing import Pool
-#     if __name__ == '__main__':
-#         with Pool(processes = 2) as file_pool:
-#             file_pool.map(resize_image, images)
-
 images = list_of_files('Source')
 resize_image(images)
-# image_threding(images)
--- PY-2.6/image_resize_mp.py
+++ PY-2.6/image_resize_mp.py
@@ -1,28 +1,33 @@
+import os
+import subprocess
+import glob
+from multiprocessing import Pool
+
+
 def resize_image(image):
-    import os, subprocess
     result_dir = os.path.join(os.getcwd(), 'Result')
-    file_name = image.split('\\')[1]
-    output_file = result_dir + '\\' + file_name + '_res200.jpg'
-    program_w_args = 'convert.exe ' + image + ' -resize 200 ' + output_file
+    output_file = os.path.join(result_dir, os.path.basename(os.path.splitext(image)[0]) + '_res200.jpg')
+    program_args = os.path.join(os.path.join(os.getcwd(), image + ' -resize 200 ' + output_file))
+    program_w_args = os.path.join(os.getcwd(), 'convert.exe ' + program_args)
+    file_name = os.path.basename(image)
     resize_it = subprocess.run(program_w_args)
     if resize_it.returncode == 0:
         print('Файл: ', file_name, ' - Выполнено успешно!')
     else:
         print('Файл: ', file_name, ' - Упс! Что-то пошло не так! Обретитесь к разработчкику.')
 
+
 def list_of_files(source_dir):
-    import glob, os.path
     image_files = glob.glob(os.path.join(source_dir, '*.jpg'))
     return image_files
 
+
 def image_threding(images):
-    import os
-    from multiprocessing import Pool
     result_dir = os.path.join(os.getcwd(), 'Result')
     if 'Result' not in os.listdir(os.getcwd()):
         os.mkdir(result_dir)
     if __name__ == '__main__':
-        with Pool(processes = 2) as file_pool:
+        with Pool(processes=2) as file_pool:
             file_pool.map(resize_image, images)
 
 
