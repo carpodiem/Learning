14892b095ca3f6ea49d5192dcfff84a265628a53
carpodiem <e-sterh@inbox.ru>
2017-06-06 12:38:26 +0300

    диплом во 2-й редакции

--- PY-Diploma/diploma.py
+++ PY-Diploma/diploma.py
@@ -25,20 +25,45 @@ params = {
     'v': VERSION,
 }
 
+pool = requests.Session()
 
-# Запись в JSON файл
+"""
+Функция записи в JSON файл
+"""
+
+
 def list_to_file(list_of_data, file_name):
     with open(file_name, 'w', encoding='utf_8') as vk_list_file:
         json.dump(list_of_data, vk_list_file, ensure_ascii=False, indent=2)
     print('Записано в файл: ', file_name)
 
 
-# Получаем список друзей пользователя
+"""
+Добавили функцию для запросов к VK.
+В качестве параметров принимает параметры для запроса и имя метода (по-умолчанию, isMember)
+"""
+
+
+def vk_request(parameters, vk_method='groups.isMember'):
+    pool.params = parameters
+    url = 'https://api.vk.com/method/' + vk_method
+    while True:
+        response = pool.get(url)
+        if response.ok:
+            return response
+        else:
+            time.sleep(2)
+
+
+"""
+Получаем список друзей пользователя
+"""
+
+
 def get_friends():
     friends_list = list()
     params['fields'] = 'nickname'
-    response = requests.get('https://api.vk.com/method/friends.get', params)
-    friends = response.json()['response']['items']
+    friends = vk_request(params, 'friends.get').json()['response']['items']
     for friend in friends:
         if 'deactivated' not in friend.keys():
             friends_list.append(friend['id'])
@@ -49,34 +74,38 @@ def get_friends():
     return friends_list
 
 
-# Получаем множество групп пользователя
+"""
+Получаем множество групп пользователя. 
+Оптимизировали ошибки 
+"""
+
+
 def get_usergroups(user_id=None):
     if user_id:
         params['user_id'] = user_id
     params['extended'] = '1'
     params['fields'] = 'members_count'
-    while True:
-        try:
-            response = requests.get('https://api.vk.com/method/groups.get', params)
-            user_groups = response.json()['response']['items']
-            groups_count = response.json()['response']['count']
-            if groups_count > 1000:
-                user_groups = []
-                n = (groups_count // 1000) + 1
-                for i in range(n):
-                    time.sleep(1.3)
-                    params['offset'] = i * 1000
-                    response = requests.get('https://api.vk.com/method/groups.get', params)
-                    user_groups.extend(response.json()['response']['items'])
-                print('Больше тысячи: ', len(user_groups))
-            break
-        except KeyError:
-            print('Слишком быстро!')
-            time.sleep(2)
+    response = vk_request(params, 'groups.get')
+    user_groups = response.json()['response']['items']
+    groups_count = response.json()['response']['count']
+    if groups_count > 1000:
+        user_groups = []
+        n = (groups_count // 1000) + 1
+        for i in range(n):
+            time.sleep(1.3)
+            params['offset'] = i * 1000
+            user_groups.extend(
+                vk_request(params, 'groups.get').json()['response']['items']
+            )
+        print('Больше тысячи: ', len(user_groups))
     return user_groups, groups_count
 
 
-# Счетчик, для того, чтобы считать количество друзей, состоящих в той же группе
+"""
+Счетчик, для того, чтобы считать количество друзей, состоящих в той же группе
+"""
+
+
 def counter(json_members):
     count = 0
     for member in json_members:
@@ -84,8 +113,12 @@ def counter(json_members):
     return count
 
 
-# Поиск по группам, смотрим с помощью метода groups.isMember, есть ли друг в той же группе, где и целевой пользователь
-def is_members(user_groups, user_friends):
+"""
+Поиск по группам, смотрим с помощью метода groups.isMember, есть ли друг в той же группе, где и целевой пользователь
+"""
+
+
+def is_members(user_groups, user_friends, friends_q):
     skeleton_groups = []
     params['user_id'] = ''
     n = (len(user_friends) // 200) + 1
@@ -108,32 +141,31 @@ def is_members(user_groups, user_friends):
                     while True:
                         try:
                             params['user_ids'] = str(user_friends[(i * 200):(i * 200 + 199)])
-                            response = requests.get('https://api.vk.com/method/groups.isMember', params)
-                            members_count += counter(response.json()['response'])
+                            members_count += counter(vk_request(params).json()['response'])
                             break
                         except KeyError:
                             time.sleep(2)
                     time.sleep(0.4)
             else:
                 params['user_ids'] = user_friends
-                response = requests.get('https://api.vk.com/method/groups.isMember', params)
-                members_count = counter(response.json()['response'])
-            if members_count == 0:
+                members_count = counter(vk_request(params).json()['response'])
+            if members_count <= friends_q:
                 skeleton_groups.append(group_id)
             bar.update(t)
     return skeleton_groups
 
 
-# Получаем список с сокращенной информацией по группам (ID, имя, количество участников)
+"""
+Получаем список с сокращенной информацией по группам (ID, имя, количество участников)
+"""
+
+
 def get_group_info(group_ids):
     params['group_ids'] = str(group_ids)
     params['fields'] = 'members_count'
-    print(params)
-    response = requests.get('https://api.vk.com/method/groups.getById', params)
-    groups = response.json()['response']
+    groups = vk_request(params, 'groups.getById').json()['response']
     groups_info = []
     for group in groups:
-        pprint(group['id'])
         groups_info.extend(
             [{
                 "id": group['id'],
@@ -144,15 +176,44 @@ def get_group_info(group_ids):
     return groups_info
 
 
-curr_user_friends = get_friends()
+"""
+Функция, запускающая последовательность функций для получения результата.
+"""
 
-curr_user_groups, curr_groups_count = get_usergroups()
-print('Количество групп пользователя: ', curr_groups_count)
-list_to_file(curr_user_groups, 'current_groups_list.json')
+def process_func(n):
+    curr_user_friends = get_friends()
+    curr_user_groups, curr_groups_count = get_usergroups()
+    print('Количество групп пользователя: ', curr_groups_count)
+    list_to_file(curr_user_groups, 'current_groups_list.json')
+    print('')
+    sk_groups = is_members(curr_user_groups, curr_user_friends, n)
+    target_groups = get_group_info(sk_groups)
+    print('')
+    print('Найдено {} групп'.format(len(target_groups)))
+    print('-' * 30)
+    print('Искомые группы:')
+    pprint(target_groups)
+    print('-' * 30)
+    list_to_file(target_groups, 'skeleton_groups_list.json')
+    print('Группы пользователя, в которых никто из его друзей не состоит, сохранены в файл!')
 
-sk_groups = is_members(curr_user_groups, curr_user_friends)
 
-target_groups = get_group_info(sk_groups)
-print(target_groups)
-list_to_file(target_groups, 'skeleton_groups_list.json')
-print('Группы пользователя, в которых никто из его друзей не состоит, сохранены в файл!')
+"""
+Главная функция запуска
+"""
+
+
+def main():
+    print(
+        'Введите количество друзей, '
+        'которые также могут состоять в одних группах с пользователем'
+        ', не более чем это количество.'
+    )
+    friends_q = input('(По-умолчанию - 0). Количество друзей = ')
+    if friends_q == '':
+        friends_q = 0
+    process_func(int(friends_q))
+
+
+if __name__ == '__main__':
+    main()
